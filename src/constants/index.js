2ad8a00403053ad78fa99b707644a5ee5c99fe62
arismihadjami <arismdj@gmail.com>
2022-07-07 21:57:35 +0800

    UAS-Mobile

---
+++ src/constants/index.js
@@ -0,0 +1,8 @@
+const APP_TOKEN = 'c6c20d5d906a7c40756fb037fb7a18f7b8d6f34e3b5fcb271753051f26dee13b'
+const REQUEST_HEADER = {
+    Accept: 'application/json',
+    'Content-Type' : 'application/json',
+    Authorization: `Bearer ${APP_TOKEN}`
+}
+
+export { REQUEST_HEADER }
